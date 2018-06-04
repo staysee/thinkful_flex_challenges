1820f8ae5a9e908529ac62d0dda4d069ccaad9e2
Stacey Sugiono <staceysugiono@gmail.com>
2018-06-04 12:47:18 -0700

    completed rendering function of app

--- shopping_list_walkthrough/index.js
+++ shopping_list_walkthrough/index.js
@@ -7,14 +7,23 @@
 const STORE = [
   {name: "apples", checked: false},
   {name: "oranges", checked: false},
-  {name: "milk", checked: false},
+  {name: "milk", checked: true},
   {name: "bread", checked: false},
 ]
 
 function generateItemElement(item, itemIndex) {
   return `
-    <li>${item.name}</li>
-  `;
+    <li class="js-item-index-element" data-item-index="${itemIndex}">
+      <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle js-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete js-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`;
 }
 
 function generateShoppingItemsString(shoppingList) {
