42e6513fd85fc60fee9938fdf23a9e42af592be8
Stacey Sugiono <staceysugiono@gmail.com>
2018-06-24 15:33:54 -0700

    render search results working

--- thinkful_tube_v2/app.js
+++ thinkful_tube_v2/app.js
@@ -14,9 +14,28 @@ function getDataFromApi(searchTerm, callback){
   $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
 
-// render result
-
 // display youtube search data
+function displayYouTubeSearchData(data){
+  const results = data.items.map((item, index) => renderResult(item));
+  console.log(data);
+  $('.js-results').html(results);
+}
+
+// render result
+function renderResult(result){
+  const videoID = result.id.videoId;
+  const channelID = result.snippet.channelId;
+
+  return `
+    <div class="search-item">
+        <a href="https://youtube.com/watch?v=${videoID}"><img src="${result.snippet.thumbnails.medium.url}"></a>
+        <div class="video-title">${result.snippet.title}</div>
+        <div class="video-description">${result.snippet.description}</div>
+        <div class="channel-title">View more by <a href="https://youtube.com/channel/${channelID}" target="_blank">${result.snippet.channelTitle}</a></div>
+    </div>
+  `
+}
+
 
 // get next page for more search results
 
@@ -29,10 +48,14 @@ function getDataFromApi(searchTerm, callback){
 function watchSubmit() {
   $('.js-search-form').submit(function(event){
     event.preventDefault();
-    console.log('submited search query');
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    console.log(`submited search query: ${query}`);
 
     //clears search field
-    $("input").val("");
+    queryTarget.val("");
+
+    getDataFromApi(query, displayYouTubeSearchData)
 
 
   })
