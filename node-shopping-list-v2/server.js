31c0aa97b356829de36df71d74b14afcb907416c
Stacey Sugiono <staceysugiono@gmail.com>
2018-09-18 12:51:09 -0700

    added POST endpoint for recipes

--- node-shopping-list-v2/server.js
+++ node-shopping-list-v2/server.js
@@ -52,6 +52,22 @@ app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
 
+app.post('/recipes', (req, res) => {
+  // ensure `name` and `ingredients` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++){
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
