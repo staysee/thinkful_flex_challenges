761d27196f7d446a3d9c28cba31523eee9848a91
Stacey Sugiono <staceysugiono@gmail.com>
2018-09-19 14:42:13 -0700

    added PUT endpoint for blogposts

--- blog_API/server.js
+++ blog_API/server.js
@@ -48,6 +48,35 @@ app.delete('/blogposts/:id', (req, res) => {
 	res.status(204).end();
 })
 
+app.put('/blogposts/:id', jsonParser, (req, res) => {
+	const requiredFields = ['id', 'title', 'content', 'author'];
+
+	for (let i=0; i<requiredFields.length; i++){
+		const field = requiredFields[i];
+
+		if (!(field in req.body)){
+			const message = `Missing \`${field}\` in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+
+	if (req.params.id !== req.body.id){
+		const message = `Request path id (${req.params.id} and request body id (${req.body.id} must match`;
+		console.error(message);
+		return res.status(400).send(message);
+	}
+
+	console.log(`Updating shopping list item \`${req.params.id}\``);
+	BlogPosts.update({
+		id: req.params.id,
+		title: req.body.title,
+		content: req.body.content,
+		author: req.body.author
+	});
+	res.status(204).end();
+})
+
 app.listen(process.env.PORT || 8080, () => {
 	console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
