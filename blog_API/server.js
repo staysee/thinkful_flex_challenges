d64b6cc2063b3d778b10bc98860f4d4a829bbc54
Stacey Sugiono <staceysugiono@gmail.com>
2018-09-19 14:31:26 -0700

    created DELETE endpoint

--- blog_API/server.js
+++ blog_API/server.js
@@ -21,6 +21,33 @@ app.get('/blogposts', (req, res) =>{
 	res.json(BlogPosts.get());
 })
 
+app.post('/blogposts', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author'];
+
+	for (let i=0; i<requiredFields.length; i++){
+		const field = requiredFields[i];
+
+		if (!(field in req.body)){
+			const message = `Missing \`${field}\` in request body`;
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+
+	const item = BlogPosts.create(
+		req.body.title,
+		req.body.content,
+		req.body.author
+	);
+	res.status(201).json(item);
+});
+
+app.delete('/blogposts/:id', (req, res) => {
+	BlogPosts.delete(req.params.id);
+	console.log(`Deleted recipe item \`${req.params.id}\``);
+	res.status(204).end();
+})
+
 app.listen(process.env.PORT || 8080, () => {
 	console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
