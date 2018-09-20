fec708a8f2ff3e7dbfe7f7fb51abc74dbfece2a9
Stacey Sugiono <staceysugiono@gmail.com>
2018-09-20 16:52:48 -0700

    working on fizz buzz test

---
+++ node-intro-to-unit-testing/test/test-fizz-buzzer.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+const fizzBuzzer = require('../fizzBuzzer');
+
+describe('fizzBuzzer', function(){
+
+	it('it should return "fizz-buzz" for multiples of 15', function(){
+
+		const normalCases
+	})
+
+	it('it should return "buzz" for multiples of 5', function(){
+		
+	})
+
+	it('it should return "fizz" for multiples of 3', function(){
+		
+	})
+
+	it('it should return the number if not multiples of 3 or 5', function(){
+		
+	})
+
+	it('it should show an error if the input is not a number', function(){
+		
+	})
+})
