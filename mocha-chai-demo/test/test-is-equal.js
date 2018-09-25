6a0b4c0db911f05425154473816c9f374f881a9d
Stacey Sugiono <staceysugiono@gmail.com>
2018-09-24 18:04:47 -0700

    some files were deleted

--- mocha-chai-demo/test/test-is-equal.js
+++ mocha-chai-demo/test/test-is-equal.js
@@ -1,14 +1,10 @@
-// import isEqual.js
 const isEqual = require('../isEqual');
 
 const expect = require('chai').expect;
 
-// "describe" is used to declare the entity to be tested, and a callback function that sets up the tests
 describe('isEqual', function() {
-    // indicate the behavior to be tested
-    // typically the "it" statement will also include a callback that provides the test
-    it('should give right answers for equal and unequal inputs', function(){
-    	 // we're going to the `isEqual` function on a range of inputs
+    it('should give right answers for equal and unequal inputs', function() {
+        // we're going to the `isEqual` function on a range of inputs
         // that the function should return true for.
         const equalInputs = [
             [1, 1],
@@ -33,7 +29,8 @@ describe('isEqual', function() {
         // loop through inputs and check that each set returns false
         unequalInputs.forEach(function(input) {
             const answer = isEqual(input[0], input[1]);
-            expect(answer).to.be.false;
+            expect(answer).to.be.true;
         });
     });
-});
+  }
+);
